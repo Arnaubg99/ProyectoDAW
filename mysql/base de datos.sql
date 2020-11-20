8102e0bb8db300f19a1e1e77d27f9c5b827c456d
Arnaubg99 <57000644+Arnaubg99@users.noreply.github.com>
2020-11-20 13:08:33 +0100

    Add files via upload

---
+++ mysql/base de datos.sql
@@ -0,0 +1,33 @@
+drop database if exists proyectoDAW;
+create database proyectoDAW;
+use proyectoDAW;
+
+create table usuario (
+id_usuario integer(10) primary key auto_increment unique,
+nombre varchar(15) not null,
+contrasena varchar(20) not null,
+correo varchar(35) not null unique,
+puntos integer(10),
+administrador boolean not null
+);
+
+create table puntuacion (
+id_usuario integer(10) primary key,
+fecha_ultima_conexion date, 
+puntos_diarios integer(10),
+puntos_maximos integer(10),
+foreign key (id_usuario) references usuario(id_usuario) on update cascade on delete cascade 
+);
+
+create table mercados (
+id_mercado integer(2) primary key auto_increment unique,
+nombre varchar(15) not null,
+url varchar(100) not null
+);
+
+create table ofertas (
+id_oferta integer(5) primary key auto_increment unique,
+nombre_comercio varchar(50) not null,
+coste_puntos integer(10) not null,
+descripcion varchar(150) not null
+);
